2eed5b9ba3da78bc599a9e67d1bb48ec2b74eacb
MiltonMario <miltonneto0010@gmail.com>
2025-11-06 21:46:56 -0300

    fix solution #1

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -8,7 +8,6 @@ function completedClass({ todo }) {
 export const TodoInfo = ({ todo }) => (
   <article className={`TodoInfo ${completedClass({ todo })}`}>
     <h2 className="TodoInfo__title">{todo.title}</h2>
-
-    <UserInfo user={todo.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
